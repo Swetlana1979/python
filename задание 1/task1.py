104c0ddf234ae404eb6a81cee0be9392e19a016a
Светлана Плотникова <uzer1874@rambler.ru>
2020-11-24 07:35:13 +0400

    Add files via upload

--- задание 1/task1.py
+++ задание 1/task1.py
@@ -1,1 +1,43 @@
+import argparse
+parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+parser.add_argument("--file",default='task.hpp')
+args = parser.parse_args()
+file = args.file
 
+
+#print(nb)
+def convert_base(nb, base):
+    
+    try:
+        if int(base):
+			base = len(base)
+        strStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        if strStr.count(str(nb))!=0:
+            nb = strStr.find(str(nb))+9
+        
+        nb = int(nb)
+        
+        alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        if nb < base:
+            return alphabet[nb]
+        else:
+            newNum = ''
+            while nb > 0:
+                newNum = str(nb % base) + newNum
+                nb //= base
+            return newNum
+                    
+    except Exception as e:
+        print('Неверный ввод')
+        
+def main (file):
+    f = open(file)
+	a = f.readline()
+	while line:
+		a = f.readline()
+	f.close()
+	nb=a[0]
+	base=a[1]
+    result = convert_base(nb, base)
+    print(result)
+main (nb=1, base='01')
